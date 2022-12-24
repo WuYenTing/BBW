05f661e08e531cc54505d04df3c8585f264bf1a1
WuYenTing <ap95071@gmail.com>
2022-12-25 02:45:23 +0800

    window size 2000*1500

--- BBW/global.h
+++ BBW/global.h
@@ -7,8 +7,8 @@
 #define grid_width 40
 #define grid_height 40
 
-#define window_width 800
-#define window_height 600
+#define window_width 2000
+#define window_height 1500
 #define field_width 600
 #define field_height 600
 
