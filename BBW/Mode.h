f37933c898adaad88b0781f92041969520a33ece
WuYenTing <ap95071@gmail.com>
2023-01-04 02:02:11 +0800

    add Mode.h

---
+++ BBW/Mode.h
@@ -0,0 +1,29 @@
+#ifndef Mode_h
+#define Mode_h
+#include "global.h"
+class Mode
+{
+public:
+
+    virtual int Mode_run();
+    virtual int Mode_update();
+
+    virtual void show_err_msg(int msg);
+    virtual void Mode_destroy();
+
+    // each drawing scene function
+    virtual void draw_Mode_map();
+    virtual void draw_Mode_item();
+
+    // process of updated event
+    virtual int process_event();
+    
+   
+
+   
+    ~Mode();
+
+public:
+    bool initial = true;
+
+};
